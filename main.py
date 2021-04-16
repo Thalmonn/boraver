538bb5450ffe63ffa00cca1e2f068d912dff15d3
Thalmonn <thalmonnsales@gmail.com>
2021-04-16 17:15:32 -0300

    Adicionado arquivo main.py do bot

---
+++ main.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+import os
+
+client = commands.Bot(command_prefix='bv!')
+token = os.getenv('DISCORD_BOT_TOKEN')
+
+@client.event
+async def on_ready():
+    await client.change_presence(status = discord.Status.online, activity = discord.Game('infos com bv!help'))
+    print('Bora Ver disponível, preparem a bilheteria!')
+    
+# Lista de comando temporários, ainda será melhor estruturado.
+    
+@client.command()
+async def ping(ctx):
+    await ctx.send('🏓 Pong com latência %s.' % str(round(client.latency, 2)))
+
+@client.command()
+async def usercheck(ctx):
+    await ctx.send('Você é o usuário: %s.' % ctx.message.author.name)
+    
+@client.command()
+@commands.is_owner()
+async def turnoff(ctx):
+    await ctx.send('Bot desligado com sucesso. Até a próxima.')
+    await client.close()
+    print('Bot desligado com sucesso. Até a próxima.')
+
+client.run(token)
